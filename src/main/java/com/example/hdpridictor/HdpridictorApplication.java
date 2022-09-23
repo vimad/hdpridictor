2bb125904748a53e4c243d9b09d9a644d6142634
Vinod Madubashana <vinod.mby@gmail.com>
2022-09-23 09:28:52 +0530

    init

---
+++ src/main/java/com/example/hdpridictor/HdpridictorApplication.java
@@ -0,0 +1,13 @@
+package com.example.hdpridictor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdpridictorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdpridictorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hdpridictor/HdpridictorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hdpridictor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HdpridictorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
